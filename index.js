4fef4dabcd0a3ba2b381753842d389d27ba5a857
Samuel <samuelh08.shm@gmail.com>
2022-03-10 18:46:21 -0500

    Adds use of emoji and colors

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-var colors = require('colors');
+const colors = require('colors');
+const emoji = require('node-emoji');
 
 console.log('OMG Rainbows!'.rainbow);
+
+console.log(emoji.random().emoji);
