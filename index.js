88898be02c0fae0880815c1f8a9eb8e85fc85f3c
Samuel <samuelh08.shm@gmail.com>
2022-03-10 19:55:15 -0500

    Adds save json file

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const colors = require('colors');
 const emoji = require('node-emoji');
+const commandLineArgs = require('command-line-args');
+const fs = require('fs');
 
-console.log('OMG Rainbows!'.rainbow);
+const filename = 'data.json';
+const filepath = `data/${filename}`;
+
+const optionDefinitions = [{ name: 'color', alias: 'c', type: String }];
+const options = commandLineArgs(optionDefinitions);
+
+colors.setTheme({ custom: [options.color] });
+
+console.log('Hoola'.custom);
 
 console.log(emoji.random().emoji);
+
+const content = JSON.stringify(options, null, 2);
+
+fs.writeFileSync(filepath, content);
